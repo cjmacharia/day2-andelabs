c6aa1dac41b68b021cc6cc752109a4e1626ea814
collins <collomash4@gmail.com>
2017-06-13 15:29:06 +0300

    andelabs datatypes and fizzbuzz working functions and test s

---
+++ fizzbuzz.py
@@ -0,0 +1,25 @@
+def fizz_buzz(n):
+    if type(n) == int:
+        if n % 3 == 0 and n % 5 != 0:
+            return 'Fizz'
+        elif n % 3 != 0 and n % 5 == 0:
+            return 'Buzz'
+        elif n % 3 == 0 and n % 5 == 0:
+            return 'FizzBuzz'
+        else:
+            return n
+    else:
+        return "Incorrect input"
+def fizz_buzz(n):
+    if type(n) == int:
+        if n % 3 == 0 and n % 5 != 0:
+            return 'Fizz'
+        elif n % 3 != 0 and n % 5 == 0:
+            return 'Buzz'
+        elif n % 3 == 0 and n % 5 == 0:
+            return 'FizzBuzz'
+        else:
+            return n
+    else:
+        return "Incorrect input"
+print (fizz_buzz(10))        
